e5cc368d5658faaf3d46ea44775140fe6b6fdfcf
Kra1o5 <kra1o5x@gmail.com>
2015-11-24 00:15:32 +0100

    Update perfd path

--- piccolo/piccolo-vendor-blobs.mk
+++ piccolo/piccolo-vendor-blobs.mk
@@ -228,7 +228,7 @@ PRODUCT_COPY_FILES += \
 
 # Perf
 PRODUCT_COPY_FILES += \
-    vendor/bq/piccolo/proprietary/bin/perfd:system/bin/perfd \
+    vendor/bq/piccolo/proprietary/vendor/bin/perfd:system/vendor/bin/perfd \
     vendor/bq/piccolo/proprietary/vendor/lib/libqti-perfd-client.so:system/vendor/lib/libqti-perfd-client.so
 
 # Postprocessing
